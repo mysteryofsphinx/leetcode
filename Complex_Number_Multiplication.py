8bb410ddafc25906d008429d0435e7de6c38ccfd
LeeHolmes <li_2787@126.com>
2017-10-29 00:31:51 +0800

    Complex_Number_Multiplication

---
+++ Complex_Number_Multiplication.py
@@ -0,0 +1,16 @@
+
+class Solution(object):
+    def complexNumberMultiply(self, a, b):
+        a1, a2 = map(int, a[:-1].split('+'))
+
+        b1, b2 = map(int, b[:-1].split('+'))
+        return '%d+%di' % (a1*b1-a2*b2, a1*b2+a2*b1)
+
+
+
+x = str(input())
+
+y = str(input())
+solution = Solution()
+ans = solution.complexNumberMultiply(x, y)
+print(ans)
