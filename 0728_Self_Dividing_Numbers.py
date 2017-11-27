f0062b719466131d91f4b969c417c0d5851ac056
LeeHolmes <li_2787@126.com>
2017-11-28 00:11:55 +0800

    a better solution

--- 0728_Self_Dividing_Numbers.py
+++ 0728_Self_Dividing_Numbers.py
@@ -23,4 +23,10 @@ left = int(input())
 right = int(input())
 solution = Solution()
 ans = solution.selfDividingNumbers(left, right)
-print(ans)        
+print(ans) 
+
+# a better solution 
+# class Solution(object):
+#     def selfDividingNumbers(self, left, right):
+#         is_self_dividing = lambda num: '0' not in str(num) and all([num % int(digit) == 0 for digit in str(num)])
+#         return filter(is_self_dividing, range(left, right + 1))       
